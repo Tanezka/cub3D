f2d38a87030d657799881261dfbd5891ea11d6e1
Tanezka <tahanezir@hotmail.com>
2024-04-19 18:20:23 +0300

    save

--- srcs/main.c
+++ srcs/main.c
@@ -73,6 +73,7 @@ int	main(int ac, char **av)
 	mlx_hook(cube.win, 3, 1L << 1, key_release, &cube);
 	mlx_hook(cube.win, 17, 1L << 17, close_win, &cube);
 	cube.img.img = mlx_new_image(cube.mlx, 1920, 1080);
+	cube.mini_player = 
 	cube.img.addr = mlx_get_data_addr(cube.img.img, &cube.img.bits_per_pixel,
 			&cube.img.line_length, &cube.img.endian);
 	init_textures(&cube);
